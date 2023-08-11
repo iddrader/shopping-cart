61a03b7f3853207ba979601dc2665c88a1155170
achytent <achytent@gmail.com>
2023-08-11 13:08:12 +0300

    Create cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,6 +5,7 @@ import '../styles/Cart.css';
 const Cart = () => {
     const [cart, setCart] = useOutletContext();
     const [products, setProducts] = useState(null);
+    const [productsToShow, setProductsToShow] = useState();
 
     useEffect(() => {
         fetch('https://fakestoreapi.com/products')
@@ -12,15 +13,56 @@ const Cart = () => {
             .then(json=>setProducts(json))
     }, [])
 
+    useEffect(() => {
+        if(cart.length === 0 || products === null){
+            setProductsToShow(null);
+            return;
+        }
+        setProductsToShow(cart.map(cartItem => {
+            const product = products.find(product => product.id === +cartItem.id);
+            product.value = cartItem.value;
+            return product;
+        }))
+    }, [products, cart])
+
+    function decrease(event) {
+        const id = event.target.dataset.id;
+        let newCart = [...cart];
+        const index = newCart.findIndex(element => element.id === id);
+        newCart[index].value -= 1;
+        if(newCart[index].value === 0)
+            newCart.splice(index, 1);
+        setCart([...newCart]);
+    }
+
+    function increase(event) {
+        const id = event.target.dataset.id;
+        const newCart = cart;
+        const index = newCart.findIndex(element => element.id === id);
+        newCart[index].value += 1;
+        setCart([...newCart]);
+    }
+
+
     return (
         <div className="cart">
+            {productsToShow && productsToShow.map(product => (
+                <div className="cart-item" key={product.id}>
+                    <img src={product.image} alt="" className="cart-image"/>
+                    <div className="cart-info">
+                        <p>{product.title}</p>
+                        <div className="quantity">
+                            <button onClick={decrease} data-id={product.id}>-</button>
+                            {product.value}
+                            <button onClick={increase} data-id={product.id}>+</button>
+                        </div>
+                    </div>
 
-            {cart.map(cartItem => (
-                <div key={cartItem.id} className="cart-item">
-                    {products && products.find(product => product.id === +cartItem.id).title}
-                    {cartItem.value}
+
                 </div>
             ))}
+
+            {!productsToShow && <div className="cart-item">Nothing here!</div>}
         </div>
     )
 }
